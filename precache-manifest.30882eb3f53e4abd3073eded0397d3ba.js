351dee8f1b4ae417acfc49c2b040b72d32a18e38
shsv382 <shsv382@gmail.com>
2019-01-30 21:41:41 +0300

    Updates

---
+++ precache-manifest.30882eb3f53e4abd3073eded0397d3ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1253087e25197a371e2f",
+    "url": "/stat/static/js/runtime~main.1253087e.js"
+  },
+  {
+    "revision": "1127136b7f24a9b55db7",
+    "url": "/stat/static/js/main.1127136b.chunk.js"
+  },
+  {
+    "revision": "51744e2a5cb5f1201173",
+    "url": "/stat/static/js/1.51744e2a.chunk.js"
+  },
+  {
+    "revision": "1127136b7f24a9b55db7",
+    "url": "/stat/static/css/main.4dc9c355.chunk.css"
+  },
+  {
+    "revision": "b055939aa22594828903f11215201316",
+    "url": "/stat/index.html"
+  }
+];
